3dc51e1a68c2edaa0adfd58f4e191a7f36a84aad
VadicJS <matyjaszczykbartek@gmail.com>
2018-02-26 15:16:18 +0100

    Api, first commit

---
+++ src/main/java/com/bartek/hospital/HospitalController.java
@@ -0,0 +1,58 @@
+package com.bartek.hospital;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bartek.doctor.DoctorService;
+
+@RestController
+public class HospitalController {
+	@Autowired
+	HospitalService hospitalService;
+
+	@Autowired
+	DoctorService doctorService;
+	
+	@GetMapping(value = "/api/hospitals")
+	public List<Hospital> getAllHospitals() {
+		return hospitalService.getAllHospitals();
+	}
+
+	@GetMapping(value = "/api/hospitals/{id}")
+	public Hospital getHospital(@PathVariable int id) {
+		return hospitalService.getHospital(id);
+	}
+
+	@PostMapping(value = "/api/hospitals")
+	public void addHospital(@Valid @RequestBody Hospital hospital){
+		
+		hospitalService.addHospital(hospital);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/api/hospitals/{id}")
+	public void updateHospital(@RequestBody Hospital hospital, @PathVariable int id) {
+		hospitalService.updateHospital(id, hospital);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/api/hospitals/{id}")
+	public void deleteHospital(@PathVariable int id) {
+		hospitalService.deleteHospital(id);
+	}
+	
+
+	
+	///@RequestMapping(method = RequestMethod.PUT, value= "/api/hospitals/{id}/doctors")
+	
+	
+	
+}
